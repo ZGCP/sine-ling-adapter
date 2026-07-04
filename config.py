0223c66381c3731382a9b6097d5af600b660c9c6
ZGCP <DLZYDYX@hotmail.com>
2026-07-04 13:17:10 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -2,8 +2,9 @@ import os
 from typing import Optional
 
 class Settings:
-    LING_API_BASE_V1: str = os.getenv("LING_API_V1", "https://market.ziling.xin/api/v1")
-    LING_API_BASE_V2: str = os.getenv("LING_API_V2", "https://market.ziling.xin/api/v2")
+    LING_API_BASE_V1: str = os.getenv("LING_API_V1", "https://api.tszxzy.dpdns.org/api/v1")
+    LING_API_BASE_V2: str = os.getenv("LING_API_V2", "https://api.tszxzy.dpdns.org/api/v2")
+    LING_DOWNLOAD_BASE: str = os.getenv("LING_DOWNLOAD_BASE", "https://api.tszxzy.dpdns.org")
     ADAPTER_HOST: str = os.getenv("HOST", "0.0.0.0")
     ADAPTER_PORT: int = int(os.getenv("PORT", os.getenv("ADAPTER_PORT", "8000")))
     
